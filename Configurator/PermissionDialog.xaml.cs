113ebe5de12f4c14278e2f8396f3508083c5f759
Eric <ebr>
2009-12-21 23:11:10 -0500

    Added Progress bar to permissions wait dialog

--- Configurator/PermissionDialog.xaml.cs
+++ Configurator/PermissionDialog.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MediaBrowser.Library.Threading;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows.Forms;
 
 namespace Configurator
 {
@@ -21,6 +25,39 @@ namespace Configurator
         public PermissionDialog()
         {
             InitializeComponent();
+            FakeProgress(progress, this);
+
         }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            StopFakeProgress();
+            base.OnClosing(e);
+        }
+
+        ManualResetEvent done = new ManualResetEvent(false);
+        ManualResetEvent exited = new ManualResetEvent(false);
+
+        private void StopFakeProgress()
+        {
+            done.Set();
+            while (!exited.WaitOne()) ;
+        }
+
+        private void FakeProgress(System.Windows.Controls.ProgressBar progress, Window window)
+        {
+            Async.Queue("Fake progress for download", () =>
+            {
+                int i = 0;
+                while (!done.WaitOne(500, false))
+                {
+                    i += 5;
+                    i = i % 100;
+                    window.Dispatcher.Invoke(DispatcherPriority.Background, (MethodInvoker)(() => { progress.Value = i; }));
+                }
+                exited.Set();
+            });
+        }
+
     }
 }
