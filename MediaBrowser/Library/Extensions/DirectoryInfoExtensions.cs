489cb1904bb0860fff1bef827d8cf9fa167306ec
Sam Saffron <sam.saffron@gmail.com>
2010-03-06 09:55:33 +1100

    Fix for http://community.mediabrowser.tv/permalinks/1807/mediabrowser-fails-to-play-some-avi-s

--- MediaBrowser/Library/Extensions/DirectoryInfoExtensions.cs
+++ MediaBrowser/Library/Extensions/DirectoryInfoExtensions.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MediaBrowser.Library.Filesystem;
+using MediaBrowser.Library.Logging;
 
 namespace MediaBrowser.Library.Extensions {
     static class DirectoryInfoExtensions {
-        public static FileInfo ToFileInfo (this System.IO.FileSystemInfo info) {
+        public static FileInfo ToFileInfo (this System.IO.FileSystemInfo info) 
+        {
+            DateTime created, modified;
+            try {
+                created = info.CreationTimeUtc;
+            } catch (Exception e) {
+                Logger.ReportException("You have a bad creation file date in the system for " + info.FullName + " this can be an issue on some linux shares", e);
+                created = DateTime.MinValue;
+            }
+            try {
+                modified = info.LastWriteTimeUtc;
+            } catch (Exception e) {
+                Logger.ReportException("You have a bad modification file date in the system for " + info.FullName + " this can be an issue on some linux shares", e);
+                modified = DateTime.MinValue;
+            }
             return new FileInfo()
             {
                 IsDirectory = info is System.IO.DirectoryInfo,
                 Path = info.FullName,
-                DateCreated = info.CreationTimeUtc,
-                DateModified = info.LastWriteTimeUtc,
+                DateCreated = created,
+                DateModified = modified,
                 Attributes = info.Attributes
             };
         }
