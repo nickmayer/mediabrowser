dfe06eea7df10b40f1acf1d85c567372f70f59dd
Eric <ebr>
2010-09-29 16:48:05 -0400

    Bump version to 2.2.9 for maint release

--- MediaBrowser/Code/Application.cs
+++ MediaBrowser/Code/Application.cs
@@ -613,6 +613,7 @@ namespace MediaBrowser
                 case "2.2.6.0":
                 case "2.2.7.0":
                 case "2.2.8.0":
+                case "2.2.9.0":
                     //set validationDelay to "0" - user can change it back if they wish or are directed to
                     Config.ValidationDelay = 0;
                     break;
