dcfe545780e20fd8e88de615cddf68eceb37a923
Birkoff <none@none>
2010-12-14 22:41:48 +0100

    MediaInfo Provider: downgraded MediaInfo dlls to 0.7.36.0; upped to version 1.1.1.0

--- MediaInfoProvider/Plugin.cs
+++ MediaInfoProvider/Plugin.cs
@@ -11,7 +11,7 @@ namespace MediaInfoProvider {
 
         internal const string PluginName = "MediaInfo Provider";
         internal const string PluginDescription = "This plugin provides rich information about your media like codecs, aspect ratio, resolution, etc. using the MediaInfo project.\n\nThis version includes MediaInfo.dll version " + Plugin.includedMediaInfoDLL + ".";
-        internal const string includedMediaInfoDLL = "0.7.37.0"; 
+        internal const string includedMediaInfoDLL = "0.7.36.0"; 
 
         public override void Init(Kernel kernel) {
             kernel.MetadataProviderFactories.Add(MetadataProviderFactory.Get<MediaInfoProvider>()); 
