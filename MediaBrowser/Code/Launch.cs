2ed869d05059d1521cd2471a9062750668585f31
Eric <ebr>
2011-03-07 11:59:16 -0500

    use mutex to block during initialization - works around a crash when two intstances are started in rapid succession

--- MediaBrowser/Code/Launch.cs
+++ MediaBrowser/Code/Launch.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using System.IO;
 using System;
 using System.Threading;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using MediaBrowser.LibraryManagement;
 using System.Xml;
 using System.Reflection;
@@ -36,34 +38,71 @@ namespace MediaBrowser
             host.MediaCenterEnvironment.Dialog("Attach debugger and hit ok", "debug", DialogButtons.Ok, 100, true); 
 #endif
 
-            var config = GetConfig();
-            if (config == null)
+            using (Mutex mutex = new Mutex(false, Kernel.MBCLIENT_MUTEX_ID))
             {
-                Microsoft.MediaCenter.Hosting.AddInHost.Current.ApplicationContext.CloseApplication();
-                return;
-            }
+                //set up so everyone can access
+                var allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+                var securitySettings = new MutexSecurity();
+                try
+                {
+                    //don't bomb if this fails
+                    securitySettings.AddAccessRule(allowEveryoneRule);
+                    mutex.SetAccessControl(securitySettings);
+                }
+                catch (Exception)
+                {
+                    //we don't want to die here and we don't have a logger yet so just go on
+                }
+                try
+                {
+                    if (mutex.WaitOne(100,false))
+                    {
 
-            Environment.CurrentDirectory = ApplicationPaths.AppConfigPath;
-            try
-            {
-                CustomResourceManager.SetupStylesMcml(host);
-                CustomResourceManager.SetupFontsMcml(host);
-            }
-            catch (Exception ex)
-            {
-                host.MediaCenterEnvironment.Dialog(ex.Message, Application.CurrentInstance.StringData("CustomErrorDial"), DialogButtons.Ok, 100, true);
-                Microsoft.MediaCenter.Hosting.AddInHost.Current.ApplicationContext.CloseApplication();
-                return;
-            }
-            using (new MediaBrowser.Util.Profiler("Total Kernel Init"))
-            {
-                Kernel.Init(config);
-            }
-            using (new MediaBrowser.Util.Profiler("Application Init"))
-            {
-                Application app = new Application(new MyHistoryOrientedPageSession(), host);
+                        var config = GetConfig();
+                        if (config == null)
+                        {
+                            Microsoft.MediaCenter.Hosting.AddInHost.Current.ApplicationContext.CloseApplication();
+                            return;
+                        }
 
-                app.GoToMenu();
+                        Environment.CurrentDirectory = ApplicationPaths.AppConfigPath;
+                        try
+                        {
+                            CustomResourceManager.SetupStylesMcml(host);
+                            CustomResourceManager.SetupFontsMcml(host);
+                        }
+                        catch (Exception ex)
+                        {
+                            host.MediaCenterEnvironment.Dialog(ex.Message, Application.CurrentInstance.StringData("CustomErrorDial"), DialogButtons.Ok, 100, true);
+                            Microsoft.MediaCenter.Hosting.AddInHost.Current.ApplicationContext.CloseApplication();
+                            return;
+                        }
+                        using (new MediaBrowser.Util.Profiler("Total Kernel Init"))
+                        {
+                            Kernel.Init(config);
+                        }
+                        using (new MediaBrowser.Util.Profiler("Application Init"))
+                        {
+                            Application app = new Application(new MyHistoryOrientedPageSession(), host);
+
+                            app.GoToMenu();
+                        }
+                        Thread.Sleep(5000); //hold our mutex a bit longer - to be sure everything is up and going
+                        mutex.ReleaseMutex();
+                    }
+                    else
+                    {
+                        //another instance running and in initialization - just blow out of here
+                        return;
+                    }
+
+                }
+                catch (AbandonedMutexException)
+                {
+                    // Log the fact the mutex was abandoned in another process, it will still get acquired
+                    Logger.ReportWarning("Previous instance of core ended abnormally...");
+                    mutex.ReleaseMutex();
+                }
             }
         }
 
