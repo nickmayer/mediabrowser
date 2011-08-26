49be41c23278e5cd93e616eee92fe28b35c8703a
Eric <ebr>
2011-08-26 15:36:19 -0400

    remove unintended double notify of failed refresh

--- MediaBrowser/Code/Application.cs
+++ MediaBrowser/Code/Application.cs
@@ -586,13 +586,6 @@ namespace MediaBrowser
                         ev.Dialog(CurrentInstance.StringData("RefreshFailedDial"), CurrentInstance.StringData("RefreshFailedCapDial"), DialogButtons.Ok, 15, true);
                         
                     }
-                    //if the service refresh failed - notify them
-                    if (Kernel.Instance.ServiceConfigData.RefreshFailed)
-                    {
-                        MediaCenterEnvironment ev = Microsoft.MediaCenter.Hosting.AddInHost.Current.MediaCenterEnvironment;
-                        ev.Dialog(CurrentInstance.StringData("RefreshFailedDial"), CurrentInstance.StringData("RefreshFailedCapDial"), DialogButtons.Ok, 15, true);
-                        
-                    }
                     // We check config here instead of in the Updater class because the Config class 
                     // CANNOT be instantiated outside of the application thread.
                     if (Config.EnableUpdates)
