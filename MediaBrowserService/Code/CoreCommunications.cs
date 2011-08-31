cd272daad7926f28700faa13f56271567085e21b
Eric <ebr>
2011-08-31 18:57:40 -0400

    Set access rights for all users on our IPC pipe.

--- MediaBrowserService/Code/CoreCommunications.cs
+++ MediaBrowserService/Code/CoreCommunications.cs
@@ -28,7 +28,14 @@ namespace MediaBrowserService
 
             Async.Queue("MB Connection", () =>
             {
-                using (NamedPipeServerStream pipe = new NamedPipeServerStream(MBServiceController.MBSERVICE_IN_PIPE, PipeDirection.In))
+                System.Security.Principal.SecurityIdentifier sid = new System.Security.Principal.SecurityIdentifier(System.Security.Principal.WellKnownSidType.BuiltinUsersSid, null); 
+
+                PipeSecurity pipeSa = new PipeSecurity();
+                pipeSa.SetAccessRule(new PipeAccessRule(sid,
+                       PipeAccessRights.ReadWrite, AccessControlType.Allow)); 
+
+
+                using (NamedPipeServerStream pipe = new NamedPipeServerStream(MBServiceController.MBSERVICE_IN_PIPE, PipeDirection.In,1,PipeTransmissionMode.Message,PipeOptions.None,1024,1024,pipeSa))
                 {
                     connected = true;
                     bool process = true;
